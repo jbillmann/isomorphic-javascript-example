643e67d9f1a41e93246c282977d66d45e3d918c7
Jeremiah Billmann <jcbillmann@yahoo.com>
2015-07-21 13:55:24 -0500

    Moved 'Views' folder to the root

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,6 @@ gulp.task('jsx', function() {
     return gulp.src('src/**/*.jsx').pipe(gulpReact()).pipe(gulp.dest('public'));
 });
 
-gulp.task('jade', function() {
-    return gulp.src('src/views/**/*.jade').pipe(gulp.dest('public/views'));
-});
-
-gulp.task('build', ['jsx', 'jade'], function() {
+gulp.task('build', ['jsx'], function() {
     return browserify('./public/client/entry.js').bundle().pipe(source('app.js')).pipe(gulp.dest('public/js'));
 });
