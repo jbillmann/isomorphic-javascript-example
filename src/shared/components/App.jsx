9fcf4cf3da0dae69bbf516c3fb46ae677cb921a1
Jeremiah Billmann <jcbillmann@yahoo.com>
2015-07-21 15:36:35 -0500

    Removed unused module & adjusted package

--- src/shared/components/App.jsx
+++ src/shared/components/App.jsx
@@ -1,7 +1,6 @@
 var React = require('react'),
     Link = require('react-router').Link,
-    RouteHandler = require('react-router').RouteHandler,
-    Home = require('./Home');
+    RouteHandler = require('react-router').RouteHandler;
 
 module.exports = React.createClass({
     render: function() {
